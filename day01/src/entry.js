e69159eae57943a79db65904b11bace032bf4537
agnee <15958021691@163.com>
2018-02-27 10:48:27 +0800

    day01

---
+++ day01/src/entry.js
@@ -0,0 +1,7 @@
+import router from './router'
+import App from '@/index.vue'
+
+/* weex initialized here, please do not move this line */
+/* eslint-disable no-new */
+new Vue(Vue.util.extend({el: '#root', router}, App))
+router.push('/')
